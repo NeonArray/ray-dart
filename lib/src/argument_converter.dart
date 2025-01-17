7e42ed279010352167d383c326211bed4ea86206
Aaron <2738518+NeonArray@users.noreply.github.com>
2025-01-17 12:23:11 -0500

    Refactor ArgumentConverter into functions

--- lib/src/argument_converter.dart
+++ lib/src/argument_converter.dart
@@ -1,21 +1,25 @@
-class ArgumentConverter {
-  static dynamic convertToPrimitive(dynamic argument) {
-    if (argument == null) {
-      return null;
-    }
+List convertValuesToPrimitives(Iterable values) {
+  return values.map((value) {
+    return convertToPrimitive(value);
+  }).toList();
+}
 
-    if (argument is String) {
-      return argument;
-    }
-
-    if (argument is int) {
-      return argument;
-    }
-
-    if (argument is bool) {
-      return argument;
-    }
-
-    return argument.toString();
+dynamic convertToPrimitive(dynamic argument) {
+  if (argument == null) {
+    return null;
   }
+
+  if (argument is String) {
+    return argument;
+  }
+
+  if (argument is num) {
+    return argument;
+  }
+
+  if (argument is bool) {
+    return argument;
+  }
+
+  return argument.toString();
 }
